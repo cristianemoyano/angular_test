96468f547fbdbb6a4e9b68d1b1b6514df2053345
cmoyano <cristianmoyano.mza@gmail.com>
2022-05-02 20:02:32 -0300

    remove appconfig

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCSBGbQJHMRlNATr6STQG6MI2pUxXAUzog',
-    authDomain: 'plazofijo-feb18.firebaseapp.com',
-    projectId: 'plazofijo-feb18',
-    storageBucket: 'plazofijo-feb18.appspot.com',
-    messagingSenderId: '548019862556',
-    appId: '1:548019862556:web:7fc8db7d1060f11373b3d9',
-    measurementId: "G-CGQDT4NZTL"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
 
